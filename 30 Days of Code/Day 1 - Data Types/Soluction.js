da34bac7435938e7a41a6fb316a49f21a7cd0e58
dantls <dan_tls@hotmail.com>
2021-01-07 21:13:00 -0400

    Add Days of Code

---
+++ 30 Days of Code/Day 1 - Data Types/Soluction.js
@@ -0,0 +1,9 @@
+function main() {
+  var i1 = readLine()
+  var d1 = readLine()
+  var s1 = readLine()
+
+  console.log(Number(i)+Number(i1))
+  console.log((Number(d)+Number(d1)).toFixed(1))
+  console.log(s+s1);
+}
